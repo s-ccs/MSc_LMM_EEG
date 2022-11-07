e336b19f3f8914d0be9e4040a91ce7152f1997ad
Luis Lips <luis@luis.fritz.box>
2022-11-07 16:06:40 +0100

    added more feedback

--- scripts/01_simulation.jl
+++ scripts/01_simulation.jl
@@ -18,10 +18,10 @@ addprocs(6)
     function simulate_data(params)
         # unpack parameters
         @unpack seed, nsubj, nitem, subj_btwn, item_btwn, both_win, β, σranef, σres, basis, fixationlen, formula, contrasts, noisetype, noiselevel = params
-        
+
         # create random number generator from seed
         rng = MersenneTwister(seed)
-        
+
         # init design, components & simulation
         design = ExperimentDesign(nsubj, nitem, subj_btwn, item_btwn, both_win)
         component = Component(basis, formula, contrasts, β, σranef, σres)
@@ -33,24 +33,22 @@ addprocs(6)
 
         # prepare parameters for logging and saving
         d = @dict seed nsubj nitem β σranef σres noisetype noiselevel
-        @show d
         map!(x->replace(string(x), string(typeof(x)) => ""), values(d))
 
-        # logging
-        time = Dates.format(now(UTC), dateformat"yyyy-mm-dd HH:MM:SS")
-        @info savename(time, d; connector=" | ", equals=" = ", sort=false, digits=2)
-
-        # add new values to dictionary params
-        ks = ["eeg", "onsets", "data", "evts"]
-        vs = [eeg, onsets, data, evts]
-        foreach(((k, v), ) -> params[k] = v, zip(ks, vs))
+        # prepare saving
+        sname = savename(d, "jld2")
+        mkpath(datadir("test"))
+        sdata = Dict("eeg" => eeg, "onsets" => onsets, "data" => data, "evts" => evts)
 
         # saving
-        sname = savename(d, "jld2")
-        mkpath(datadir("simulation"))
-        wsave(datadir("simulation", sname), params)
-       
-        return true
+        wsave(datadir("test", sname), sdata)
+        
+        # logging
+        time = Dates.format(now(UTC), dateformat"yyyy-mm-dd HH:MM:SS")
+        info = "$(myid()):$(gethostname()) : "
+        @info savename(info*time, d; connector=" | ", equals=" = ", sort=false, digits=2)
+
+        return params
     end
 
     """
@@ -72,7 +70,31 @@ cfg = config(args["filename"])
 
 # Split config into list of parameter combinations
 dicts = dict_list(cfg)
+n_dicts = dict_list_count(cfg)
 
 # Simulate data
-@info "Simulating data..."
+@info "Simulating data... $(n_dicts)"
 res = pmap(simulate_data, dicts)
+
+# Removing workers
+rmprocs(workers())
+
+# check for missing
+dicts = [filter(k->k.first ∉ ["formula", "contrasts"], d) for d in dicts]
+res = [filter(k->k.first ∉ ["formula", "contrasts"], r) for r in res]
+missing = filter(!in(res), dicts)
+
+# result
+if isempty(missing)
+    @info "Successfully simulated $(n_dicts) simulations!"
+else
+    @warn "$(length(missing)) simulations missing!"
+    for m in dicts
+        @unpack seed, nsubj, nitem, β, σranef, σres, noisetype, noiselevel = m
+        d = @dict seed nsubj nitem β σranef σres noisetype noiselevel
+        map!(x->replace(string(x), string(typeof(x)) => ""), values(d))
+        @warn savename(d; connector=" | ", equals=" = ", sort=false, digits=2)
+    end
+end
+
+
